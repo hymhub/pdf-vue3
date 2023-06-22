d08c56ba566d569233f95db77a96b4e3b6c3b0eb
hymenhan <hyminghan@qq.com>
2023-06-22 15:10:19 +0800

    fix: git pages

--- pdf-vue3-example/vite.config.js
+++ pdf-vue3-example/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/pdf-vue3/',
   server: {
     host:'0.0.0.0',
     cors: true,
