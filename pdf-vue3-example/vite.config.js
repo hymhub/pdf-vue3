74c3ec5c72954c040dec2f57bdcd4a2ba7e7f54f
hyming <1749207188@qq.com>
2026-05-19 22:50:36 +0800

    chore: update Node.js version to 20.x and add path alias for pdf-vue3 in Vite and Nuxt configurations

--- pdf-vue3-example/vite.config.js
+++ pdf-vue3-example/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'pdf-vue3': fileURLToPath(new URL('../src/index.ts', import.meta.url)),
+    },
+  },
   base: '/pdf-vue3/',
   server: {
     host:'0.0.0.0',
